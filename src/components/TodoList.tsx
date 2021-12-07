220fd6545c9fa05e4c614403b6e39577849b5061
Ross Acheson <rossacheson@users.noreply.github.com>
2021-12-07 18:18:18 -0500

    Update context state

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class TodoList extends Component {
   static contextType = ThemeContext;
   render() {
-    const { isDarkTheme, darkTheme, lightTheme } = this.context;
+    const { isDarkTheme, darkTheme, lightTheme, changeTheme } = this.context;
     const theme = isDarkTheme ? darkTheme : lightTheme;
 
-    const { todoContainer, item } = styles;
+    const { todoContainer, item, buttonContainer, buttonText } = styles;
 
     return (
       <View style={[todoContainer, theme]}>
         <Text style={[item, theme]}>Plan the family trip</Text>
         <Text style={[item, theme]}>Go shopping for dinner</Text>
         <Text style={[item, theme]}>Go for a walk</Text>
+        <TouchableOpacity style={buttonContainer} onPress={changeTheme}>
+          <Text style={buttonText}>Change Theme</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -31,6 +34,17 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 18,
     paddingVertical: 10,
+  },
+  buttonContainer: {
+    backgroundColor: 'dodgerblue',
+    paddingVertical: 10,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
   },
 });
 
